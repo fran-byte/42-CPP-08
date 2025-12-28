a52ed4ea83b73ce4295e9fda40ad70fcd4498cbd
frrommero <frromero@student.42madrid.com>
2025-12-28 16:53:32 +0100

    up

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,12 +6,14 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/27 20:36:37 by frromero          #+#    #+#             */
-/*   Updated: 2025/12/27 20:53:57 by frromero         ###   ########.fr       */
+/*   Updated: 2025/12/28 16:47:35 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
+
+#include <algorithm>
 
 template <typename T>
 typename T::iterator easyfind(T &container, int value);
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/27 21:11:15 by frromero          #+#    #+#             */
-/*   Updated: 2025/12/28 16:34:35 by frromero         ###   ########.fr       */
+/*   Updated: 2025/12/28 16:51:09 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,12 @@ int main()
     std::vector<int> myContainer;
     std::vector<char> myContainer2;
     
-    for(int i = 0; i <10 ; i=i+2)
-        myContainer.push_back(i*2);
-    
-    for(int i = 32; i < 127; i++)
-          myContainer2.push_back(static_cast<char>(i));
+    for(int i = 0; i <=10 ; i=i+2)
+        myContainer.push_back(i*2);   
 
 
-    std::vector<int>::iterator it = easyfind(myContainer, 8);
-
-    std::vector<char>::iterator it2 = easyfind(myContainer2, 'a');
-
+    std::vector<int>::iterator it = std::find(myContainer.begin(), myContainer.end(), 8);
     std::cout << "myContainer <int>  iterator point to : " << *it << std::endl;
-    std::cout << "myContainer <char> iterator point to : " << *it2 << std::endl;
+
     return 0;
 }
