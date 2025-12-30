81df9f02b17c74d7a3f90df7b3f2f49cb355aa97
fran-byte <fran-byte@hotmail.com>
2025-12-30 16:15:47 +0100

    up

---
+++ ex01/Span.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/30 15:51:11 by frromero          #+#    #+#             */
+/*   Updated: 2025/12/30 16:15:18 by frromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+#define SPAN_HPP
+#include <vector>
+#include <exception>
+
+class Span
+{
+private:
+    unsigned int _maxN;
+    std::vector<int> container;
+
+public:
+    Span(void);
+    Span(unsigned int maxN);
+    Span(Span const &copy);
+    ~Span(void);
+    Span &operator=(Span const &other);
+    void addNumber(int number);
+    int shortestSpan() const;
+    int longestSpan() const;
+    class SpanFullException : public std::exception
+    {
+    public:
+        const char *what() const throw();
+    };
+
+    class NoSpanException : public std::exception
+    {
+    public:
+        const char *what() const throw();
+    };
+};
+
+#endif
