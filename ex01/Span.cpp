6bc2edf3cbc380192926e4e8d436f32a20c49161
fran-byte <fran-byte@hotmail.com>
2025-12-31 13:34:46 +0100

    up

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: p4c0 <p4c0@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 16:15:31 by frromero          #+#    #+#             */
-/*   Updated: 2025/12/31 13:29:10 by p4c0             ###   ########.fr       */
+/*   Updated: 2025/12/31 13:34:23 by p4c0             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,15 @@ Span::Span(unsigned int maxN) : _maxN(maxN)
 Span::Span(Span const &copy) 
     : _maxN(copy._maxN), _container(copy._container) 
 {}
+
+Span::~Span(void){}
+
+Span &Span::operator=(Span const &other)
+{
+    if (this != &other)
+    {
+        _maxN = other._maxN;
+        _container = other._container;
+    }
+    return *this;
+}
